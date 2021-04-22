319e327877790851a7417fa2c8bd7b3ee9974748
markry11 <kryska.marek@gmail.com>
2021-04-22 19:05:46 +0200

    refactor: structure of project reworked

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='pywindowsmediacontrol',
+    name='windows_media_control',
     version='0.0.1',
     description='package allowing to add callbacks to winRT Windows.Media.Control events',
-    url='git@github.com:markry11/pyWindowsMediaControl.git',
+    url='git@github.com:markry11/windows-media-control.git',
     author='Marek Kryska',
     author_email='kryska.marek@gmail.com',
     license='MIT',
-    packages=['pywindowsmediacontrol'],
+    packages=['windows_media_control'],
     zip_safe=False
 )
